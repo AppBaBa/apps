cce80fe5eb6d0d105e72cf92b991032343a21532
appbaba <developer.tvirus@gmail.com>
2024-06-12 11:42:00 +0530

    Add Records

--- learn.dart
+++ learn.dart
@@ -1,16 +1,11 @@
-import 'dart:math' as math;
-
 void main() {
   print(cities.London.differs);
-  String s = 'Hello Flutter';
-  print('Hello + $s');
-  List<int> lists = [1, 2, 3, 4, 5];
-  print('List integer +$lists');
-  final listA = [1, 2, 3];
-  final listB = [1, 2, 3];
-  print(listA == listB);
-  final size = 2 * (6 / math.sqrt2);
-  print(size);
+  var uses = getUser();
+  print(uses);
+}
+
+(String id, String name) getUser() {
+  return ('Amit', 'Name');
 }
 
 enum cities { Delhi, Mumbai, London, Noida, Goa }
