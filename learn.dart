041bce1e576af5086b6a77ea8c5be9f07d56f5cb
appbaba <developer.tvirus@gmail.com>
2024-06-12 14:16:02 +0530

    Add RSA

--- learn.dart
+++ learn.dart
@@ -2,6 +2,8 @@ void main() {
   print(cities.London.differs);
   var uses = getUser();
   print(uses);
+  var record = ('Flutter', a: 12, b: true, "Amit");
+  print(record.$1);
 }
 
 (String id, String name) getUser() {
