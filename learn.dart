51501619bb6ae1a6abb29bff1ef943cc211f8835
appbaba <developer.tvirus@gmail.com>
2024-06-12 14:28:52 +0530

    Add List for loop

--- learn.dart
+++ learn.dart
@@ -4,6 +4,9 @@ void main() {
   print(uses);
   var record = ('Flutter', a: 12, b: true, "Amit");
   print(record.$1);
+  var listOfInts = [1, 2, 3, 4];
+  var listString = [for (var i in listOfInts) '$i'];
+  print(listString);
 }
 
 (String id, String name) getUser() {
