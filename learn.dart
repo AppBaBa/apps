cd3dda7f266527b5c6d4233fd48960cf2a828ab0
appbaba <developer.tvirus@gmail.com>
2024-06-12 10:32:42 +0530

    List Compare

--- learn.dart
+++ learn.dart
@@ -4,6 +4,9 @@ void main() {
   print('Hello + $s');
   List<int> lists = [1, 2, 3, 4, 5];
   print('List integer +$lists');
+  final listA = [1, 2, 3];
+  final listB = [1, 2, 3];
+  print(listA == listB);
 }
 
 enum cities { Delhi, Mumbai, London, Noida, Goa }
